af7da4f5f5daa9e145637ef7cc8a922224c4bdcc
Franco Serio <franco@samser.co>
2023-12-24 17:11:26 -0300

    feat: Creating the contest

--- src/CreatingTheContest.cpp
+++ src/CreatingTheContest.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 
 int main() {
-    int cases;
-    cin >> cases;
-    int difficulty;
-    int res = 1;
-    int strikeAtTheMoment = 1;
+  int cases;
+  cin >> cases;
+  int difficulty;
+  int res = 1;
+  int strikeAtTheMoment = 1;
+  cin >> difficulty;
+  int before = difficulty;
+  for (int i = 1; i < cases; ++i) {
     cin >> difficulty;
-    int before = difficulty;
-    for (int i = 1; i < cases; ++i) {
-        cin >> difficulty;
-        if (difficulty <= before*2) {
-            strikeAtTheMoment++;
-        } else {
-            strikeAtTheMoment = 1;
-        }
-        if (strikeAtTheMoment > res) {
-            res = strikeAtTheMoment;
-        }
-        before = difficulty;
+    if (difficulty <= before * 2) {
+      strikeAtTheMoment++;
+    } else {
+      strikeAtTheMoment = 1;
     }
+    if (strikeAtTheMoment > res) {
+      res = strikeAtTheMoment;
+    }
+    before = difficulty;
+  }
 
-    cout << res << endl;
+  cout << res << endl;
 
-    return 0;
+  return 0;
 }
